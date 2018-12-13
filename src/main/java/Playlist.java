2a356c561a0b83d18a821cc3334456902b68cc51
szymanskimichal90 <szymanskimichal90@gmail.com>
2018-12-13 22:15:22 +0100

    Working on sorting list.

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -14,18 +14,31 @@ public class Playlist implements Track {
     }
 
     private String name;
-    private Set<Track> playlist = new LinkedHashSet<Track>();
+    private List<Track> playlist = new ArrayList<>();
+
 
     void addTrackToPlst(Track track) {
         playlist.add(track);
     }
 
+    public void setHowToPlay(TypeOfPlay toPlay) {
+        if (toPlay == TypeOfPlay.RANDOM) {
+            Collections.shuffle(playlist);
+        }
+        else if (toPlay == TypeOfPlay.LOOPED) {
+            playlist.add((Track) playlist);}
+           else if (toPlay == TypeOfPlay.SEQUENTIAL) {
+                Collections.unmodifiableList(playlist);
+            }
 
+        }
+
+    }
 
     @Override
     public void play() {
-        System.out.println("You are listening whole playlist. Name:"+name);
-        for (Track t:playlist
+        System.out.println("You are listening whole playlist. Name:" + name);
+        for (Track t : playlist
         ) {
             t.play();
 
